425c36fb6fb10fa670659e58a90f7638c13bdcc9
Aqib <aqibr777@gmail.com>
2022-11-16 23:09:22 +0500

    4. Inertia-links

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -3,16 +3,18 @@
 
     <p>In this series, we will use the following frameworks:</p>
 
-    <ul>
-      <li v-for="framework of frameworks" v-text="framework"></li>
-    </ul>
+    <Nav />
   </template>
 
   <script>
+
+import Nav from '../Shared/Nav';
+
   export default {
     props: {
       name: String,
       frameworks: Array,
     },
+    components: { Nav },
   };
   </script>
