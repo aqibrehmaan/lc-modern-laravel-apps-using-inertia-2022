7fafd850f792379e06fb99f994585f433f0e1e49
Aqib <aqibr777@gmail.com>
2022-11-27 01:00:40 +0500

    15-dynamic-head-and-meta-tags,16-The-most-important-spa-security-concern

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
@@ -21,7 +22,9 @@ Route::get('/', function () {
 Route::get('/users', function () {
 
     return Inertia::render('Users', [
-        'time' => now()->toTimeString()
+        'users' => User::all()->map(fn($user) => [
+            'name' => $user->name
+        ])
     ]);
 });
 
