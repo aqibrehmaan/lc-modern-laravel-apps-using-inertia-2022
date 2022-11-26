d42696268c973f8c046cc65424057da9b36b0824
Aqib <aqibr777@gmail.com>
2022-11-27 01:10:29 +0500

    18-Filtering

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,6 +1,10 @@
 <template>
   <Head title="Users" />
-  <h1 class="text-3xl mb-6">Users</h1>
+  <div class="flex justify-between mb-6">
+    <h1 class="text-3xl">Users</h1>
+
+    <input v-model="search" type="text" placeholder="Search..." class="border px-2 rounded-lg" />
+  </div>
   <div class="flex flex-col">
     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
       <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
@@ -19,9 +23,7 @@
                 </td>
 
                 <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                  <Link :href="`/users/${user.id}/edit`" class="text-indigo-600 hover:text-indigo-900">
-                    Edit
-                  </Link>
+                    <Link :href="`/users/${user.id}/edit`" class="text-indigo-600 hover:text-indigo-900"> Edit </Link>
                 </td>
               </tr>
             </tbody>
@@ -35,6 +37,18 @@
 </template>
 
 <script setup>
-import Pagination from "../Shared/Pagination";
-defineProps({ users: Object })
+import Pagination from '../Shared/Pagination';
+import { ref, watch } from "vue";
+import {Inertia} from "@inertiajs/inertia";
+let props = defineProps({
+  users: Object,
+  filters: Object
+});
+let search = ref(props.filters.search);
+watch(search, value => {
+  Inertia.get('/users', { search: value }, {
+    preserveState: true,
+    replace: true
+  });
+});
 </script>
