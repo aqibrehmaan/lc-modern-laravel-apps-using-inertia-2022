1f12ed7d9b7637b7e308b1a6dc456faad7c40642
Aqib <aqibr777@gmail.com>
2022-11-27 00:53:28 +0500

    14-code-splitting-and-dynamic-page-imports

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,19 +1,13 @@
 <template>
-    <h1 class="text-3xl">
-      Users
-    </h1>
-    <div style="margin-top: 400px">
-        <p>The current time is {{ time }}.</p>
+  <Head title="Users" />
+  <h1 class="text-3xl">Users</h1>
+  <div style="margin-top: 400px">
+    <p>The current time is {{ time }}.</p>
 
-        <Link href="/users" class="text-blue-500" preserve-scroll>
-            Refresh
-        </Link>
-    </div>
+    <Link href="/users" class="text-blue-500" preserve-scroll> Refresh </Link>
+  </div>
 </template>
 
-<script>
-
-export default {
-    props: { time: String },
-};
+<script setup>
+defineProps({ time: String });
 </script>
--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -1,27 +1,26 @@
 <template>
-    <section class="p-6 bg-gray-200">
-        <header class="flex justify-between">
-            <div class="flex items-center">
-                <h1 class="font-bold text-lg">My App</h1>
+  <Head>
+    <meta
+      type="description"
+      content="Information about my app"
+      head-key="description"
+    />
+  </Head>
 
-                <p class="text-sm ml-4">Welcome Back, {{ username }}!</p>
-            </div>
+  <section class="p-6 bg-gray-200">
+    <header class="flex justify-between">
+      <div class="flex items-center">
+        <h1 class="font-bold text-lg">My App</h1>
 
-            <Nav />
-        </header>
-    </section>
+        <p class="text-sm ml-4">Welcome Back, {{ username }}!</p>
+      </div>
 
-    <section class="p-6">
+      <Nav />
+    </header>
+  </section>
+
+  <section class="p-6">
     <div class="max-w-3xl mx-auto">
-      <iframe
-        class="mb-6"
-        width="100%"
-        frameborder="no"
-        scrolling="no"
-        seamless
-        src="https://player.simplecast.com/fd0bd2ba-c553-466c-a060-b144797ce369?dark=false"
-      />
-
       <slot />
     </div>
   </section>
@@ -30,11 +29,11 @@
 <script>
 import Nav from "./Nav";
 export default {
-    components: { Nav },
-    computed: {
-        username() {
-            return this.$page.props.auth.user.username;
-        },
+  components: { Nav },
+  computed: {
+    username() {
+      return this.$page.props.auth.user.username;
     },
+  },
 };
 </script>
