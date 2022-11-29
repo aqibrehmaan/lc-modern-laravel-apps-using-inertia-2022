30ffb9b11174d12e4a6f2fc8f3e65d8b94bd32c4
Aqib <aqibr777@gmail.com>
2022-11-29 23:58:16 +0500

    Clean up routes and refactor to controllers

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Models\User;
-use Inertia\Inertia;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Request;
+use App\Http\Controllers\UsersController;
+use Illuminate\Support\Facades\Route;;
 use App\Http\Controllers\Auth\LoginController;
 
 Route::get('login', [LoginController::class, 'create'])->name('login');
@@ -12,50 +9,12 @@ Route::post('login', [LoginController::class, 'store']);
 Route::post('logout', [LoginController::class, 'destroy'])->middleware('auth');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/', function () {
-        return Inertia::render('Home');
-    });
+    
+    Route::inertia('/', 'Home');
+    Route::inertia('/settings', 'Settings');
 
-    Route::get('/users', function () {
-        return Inertia::render('Users/Index', [
-            'users' => User::query()
-                ->when(Request::input('search'), function ($query, $search) {
-                    $query->where('name', 'like', "%{$search}%");
-                })
-                ->paginate(10)
-                ->withQueryString()
-                ->through(fn ($user) => [
-                    'id' => $user->id,
-                    'name' => $user->name,
-                    'can' => [
-                        'edit' => Auth::user()->can('edit', $user)
-                    ]
-                ]),
+    Route::get('/users', [UsersController::class, 'index']);
+    Route::get('/users/create', [UsersController::class, 'create']);
+    Route::post('/users', [UsersController::class, 'store']);
 
-                'filters' => Request::only(['search']),
-                'can' => [
-                    'createUser' => Auth::user()->can('create', User::class)
-                ]
-        ]);
-    });
-
-    Route::get('/users/create', function () {
-        return Inertia::render('Users/Create');
-    })->can('create', 'App\Models\User');
-
-    Route::post('/users', function () {
-        $attributes = Request::validate([
-            'name' => 'required',
-            'email' => ['required', 'email'],
-            'password' => 'required',
-        ]);
-
-        User::create($attributes);
-
-        return redirect('/users');
-    });
-
-    Route::get('/settings', function () {
-        return Inertia::render('Settings');
-    });
 });
